d0dc8981e9c97794ba5d2629cd440e1372865afe
deviousquacks23 <123861902+DeviousQuacks23@users.noreply.github.com>
2025-06-22 17:43:44 +0800

    Updated the loadscreen

--- loadscreen.lua
+++ loadscreen.lua
@@ -1,6 +1,7 @@
 local time = 0
 local image = Graphics.loadImage("loadscreen.png")
 local rot = 0
+local fadeOut = 0
 
 function onDraw()    
 	if image == nil then
@@ -18,9 +19,26 @@ function onDraw()
 		height = 64,
 		sourceWidth = 64,
 		sourceHeight = 64,
-        rotation = rot,
+        	rotation = rot,
 		centered = true,
 		sceneCoords = false,
-        color = {1, 1, 1, math.min(1, time / 48)},
+        	color = {1, 1, 1, math.min(1, time / 60)},
 	}
+
+        if Misc.getLoadingFinished() then
+            	fadeOut = math.min(1,fadeOut + 1/10)
+        end
+
+        if fadeOut < 1 then
+            	Misc.setLoadScreenTimeout(15)
+        else
+            	Misc.setLoadScreenTimeout(0)
+        end
+
+    	if fadeOut > 0 then
+        	Graphics.drawBox{
+            		color = {0, 0, 0, fadeOut},priority = 15,
+            		x = 0,y = 0,width = 512,height = 448,
+        	}
+    	end
 end
