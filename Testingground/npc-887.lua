c5e50aaf2313d4d2a12c38ca3a37ba5acbf3eeee
NessWednesday <143552864+NessWednesday@users.noreply.github.com>
2025-07-23 09:31:17 -0400

    Testingground
    Added whatever I finished on the 11th.

---
+++ Testingground/npc-887.lua
@@ -0,0 +1,114 @@
+--NPCManager is required for setting basic NPC properties
+local npcManager = require("npcManager")
+local sidestepper = require("AI/sidestepper")
+
+--Create the library table
+local sampleNPC = {}
+--NPC_ID is dynamic based on the name of the library file
+local npcID = NPC_ID
+
+--Defines NPC config for our NPC. You can remove superfluous definitions.
+local sampleNPCSettings = {
+	id = npcID,
+	--Sprite size
+	gfxwidth = 32,
+	gfxheight = 32,
+	--Hitbox size. Bottom-center-bound to sprite size.
+	width = 32,
+	height = 32,
+	--Sprite offset from hitbox for adjusting hitbox anchor on sprite.
+	gfxoffsetx = 0,
+	gfxoffsety = 2,
+	--Frameloop-related
+	frames = 6,
+	framestyle = 0,
+	framespeed = 8, --# frames between frame change
+	--Movement speed. Only affects speedX by default.
+	speed = 1,
+	luahandlespeed = true,
+	--Collision-related
+	npcblock = false,
+	npcblocktop = false, --Misnomer, affects whether thrown NPCs bounce off the NPC.
+	playerblock = false,
+	playerblocktop = false, --Also handles other NPCs walking atop this NPC.
+
+	nohurt=false,
+	nogravity = false,
+	noblockcollision = false,
+	nofireball = false,
+	noiceball = false,
+	noyoshi= false,
+	nowaterphysics = false,
+	--Various interactions
+	jumphurt = true, --If true, spiny-like
+	spinjumpsafe = true, --If true, prevents player hurt when spinjumping
+	harmlessgrab = false, --Held NPC hurts other NPCs if false
+	harmlessthrown = false, --Thrown NPC hurts other NPCs if false
+
+	grabside=false,
+	grabtop=false,
+	
+	flippedNPC=888,
+	advancedNPC=887,
+	isFlipped=false,
+
+	--Identity-related flags. Apply various vanilla AI based on the flag:
+	--iswalker = false,
+	--isbot = false,
+	--isvegetable = false,
+	--isshoe = false,
+	--isyoshi = false,
+	--isinteractable = false,
+	--iscoin = false,
+	--isvine = false,
+	--iscollectablegoal = false,
+	--isflying = false,
+	--iswaternpc = false,
+	--isshell = false,
+
+	--Emits light if the Darkness feature is active:
+	--lightradius = 100,
+	--lightbrightness = 1,
+	--lightoffsetx = 0,
+	--lightoffsety = 0,
+	--lightcolor = Color.white,
+
+	--Define custom properties below
+}
+
+--Applies NPC settings
+npcManager.setNpcSettings(sampleNPCSettings)
+
+--Register the vulnerable harm types for this NPC. The first table defines the harm types the NPC should be affected by, while the second maps an effect to each, if desired.
+npcManager.registerHarmTypes(npcID,
+	{
+		--HARM_TYPE_JUMP,
+		HARM_TYPE_FROMBELOW,
+		HARM_TYPE_NPC,
+		--HARM_TYPE_PROJECTILE_USED,
+		HARM_TYPE_LAVA,
+		HARM_TYPE_HELD,
+		HARM_TYPE_TAIL,
+		--HARM_TYPE_SPINJUMP,
+		--HARM_TYPE_OFFSCREEN,
+		HARM_TYPE_SWORD
+	}, 
+	{
+		--[HARM_TYPE_JUMP]=10,
+		[HARM_TYPE_FROMBELOW]=33,
+		[HARM_TYPE_NPC]=33,
+		--[HARM_TYPE_PROJECTILE_USED]=10,
+		[HARM_TYPE_LAVA]={id=13, xoffset=0.5, xoffsetBack = 0, yoffset=1, yoffsetBack = 1.5},
+		[HARM_TYPE_HELD]=33,
+		[HARM_TYPE_TAIL]=33,
+		[HARM_TYPE_SPINJUMP]=10,
+		--[HARM_TYPE_OFFSCREEN]=10,
+		[HARM_TYPE_SWORD]=10,
+	}
+);
+
+--Custom local definitions below
+sidestepper.register(npcID)
+
+--Gotta return the library table!
+return sampleNPC
