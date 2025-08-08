e4d07bdfc279bdab791823fe703825df5d1d2766
MegaDood11 <megadood11@gmail.com>
2025-08-08 22:13:46 +1000

    Removed some leftover code

--- bonkFlipsMushrooms.lua
+++ bonkFlipsMushrooms.lua
@@ -2,18 +2,7 @@ local ground = {}
 
 --Register events
 function ground.onInitAPI()
-	registerEvent(ground, "onTick")
 	registerEvent(ground, "onBlockHit")
-end
-
-function ground.onTick()
-	if player.keys.altRun == KEYS_PRESSED and player.keys.up then 
-		if player.character == ground.character1 then
-			player.character = ground.character2
-		else
-			player.character = ground.character1
-		end
-	end
 end
 
 function ground.onBlockHit(e, v, upper, p)
