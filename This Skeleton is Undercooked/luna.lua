eb9c68acab697bf55aa136aa194bba1717bdb6da
MegaDood11 <megadood11@gmail.com>
2025-08-18 23:10:43 +1000

    Added -2

---
+++ This Skeleton is Undercooked/luna.lua
@@ -0,0 +1,19 @@
+local smb1HUD = require("smb1HUD")
+
+smb1HUD.currentWorld = vector("",2)
+-- Run code on level start
+function onStart()
+    --Your code here
+end
+
+-- Run code every frame (~1/65 second)
+-- (code will be executed before game logic will be processed)
+function onTick()
+    --Your code here
+end
+
+-- Run code when internal event of the SMBX Engine has been triggered
+-- eventName - name of triggered event
+function onEvent(eventName)
+    --Your code here
+end
