9b4af805c584ef64ca2eb3593a80792b36c9b673
MegaDood11 <megadood11@gmail.com>
2025-10-18 13:29:38 +1100

    f

--- bonusChars.lua
+++ bonusChars.lua
@@ -5,7 +5,11 @@ bonusChars.bgoID = 139
 bonusChars.characterImages = {
 	Graphics.loadImageResolved("bonusImages/mario.png"),
 	Graphics.loadImageResolved("bonusImages/luigi.png"),
+<<<<<<< HEAD
 	Graphics.loadImageResolved("bonusImages/peach.png"),
+=======
+	Graphics.loadImageResolved("bonusImages/waaa.png"),
+>>>>>>> b4337f02c879b6acf29cec93187acb128e2c72f9
 	Graphics.loadImageResolved("bonusImages/toad.png"),
 	nil, -- link
 	nil, -- megaman
