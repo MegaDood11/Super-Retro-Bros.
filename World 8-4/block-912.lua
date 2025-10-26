5ab93a6458f02482d092b724ae067c1286618c14
MegaDood11 <megadood11@gmail.com>
2025-10-26 20:40:09 +1100

    Added 8-4

---
+++ World 8-4/block-912.lua
@@ -0,0 +1,20 @@
+local blockmanager = require("blockmanager")
+local escalator = require("blocks/ai/escalator")
+
+local blockID = BLOCK_ID
+
+local block = {}
+
+blockmanager.setBlockSettings({
+	id = blockID,
+	frames = 1,
+	framespeed = 1,
+	speed = 2,
+	sizable = false,
+	semisolid = false,
+	direction = 1
+})
+
+escalator.register(blockID)
+
+return block
