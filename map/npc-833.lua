e4a34852a8a6f21d0da7705df70844866c0bcee7
deviousquacks23 <duchluong1603@gmail.com>
2026-03-14 23:04:20 +0800

    finshed the minus warp

---
+++ map/npc-833.lua
@@ -0,0 +1,27 @@
+--[[
+
+    smwMap.lua
+    by MrDoubleA
+
+    See main file for more
+
+]]
+
+local smwMap = require("smwMap")
+
+
+local npcID = NPC_ID
+local obj = {}
+
+
+smwMap.setObjSettings(npcID,{
+    framesY = 1,
+
+  
+
+    isLevel = true,
+    isWarp = true,
+})
+
+
+return obj
